f0c707d18be3ef10cf7206bba12969083f20b402
Akash Kaintura <akashkaintura@Akashs-MacBook-Pro.local>
2021-09-04 13:47:26 +0530

    Fix: extra input field

--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const Author = require('../models/author');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
 
 // All Authors Route
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if(req.query.name != null && req.query.name != ''){
-        searchOptions.name = new RegExp(req.query.name, 'i')
+    if(req.query.name != null && req.query.name !== ''){
+       searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
         const authors = await Author.find(searchOptions)
@@ -24,13 +25,14 @@ router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() });
 });
 
-// Create Author  Route
+// Create Author Route
 router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
     try {
         const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`)
         res.redirect(`authors`)
     } catch (error) {
         res.render('authors/new', {

c69f43e33a2db21867f9c71295eaa8af1f7fa56c
Akash Kaintura <akashkaintura@Akashs-MacBook-Pro.local>
2021-09-04 13:23:04 +0530

    Initial routes and search RegExp

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,20 @@ const Author = require('../models/author');
 const router = express.Router();
 
 // All Authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name != ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch  {
+        res.redirect('/')
+    }    
 });
 
 // New Author
@@ -13,20 +25,19 @@ router.get('/new', (req, res) => {
 });
 
 // Create Author  Route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
-    author.save((err, newAuthor) => {
-        if(err){
-            res.render('authors/new', {
-                author: author,
-                errorMessage: 'Error creating Author'
-            })
-        }else{
-            res.redirect(`authors`)
-        }
-    })
+    try {
+        const newAuthor = await author.save();
+        res.redirect(`authors`)
+    } catch (error) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
 });
 
 module.exports = router

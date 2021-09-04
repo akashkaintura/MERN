7cce99bf1f50562638f5dc0468f72b9d555a022f
Akash Kaintura <akashkaintura@Akashs-MacBook-Pro.local>
2021-09-04 16:41:46 +0530

    update && fix: new changes

--- routes/books.js
+++ routes/books.js
@@ -1,27 +1,12 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router()
-const path = require('path')
 const Book   = require('../models/book')
 const Author = require('../models/author')
-const fs = require('fs')
 
-const uploadPath = path.join('public', Book.coverImageBasePath)
-
-// Image mime type
-// const imageMimeType = ['images/jpeg', 'images/png', 'images/gif']
-
-// Multer
-// const upload = multer({
-//     dest: uploadPath,
-//     fileFilter: (req, file, callback) =>{
-//         callback(null, imageMimeType.includes(file.mimetype))
-//     }
-// })
+const imageMimeTypes = ['images/png', 'images/jpeg', 'images/gif']
 
 // All Books Route
 router.get('/', async (req, res) => {
-
-    // matching Query
     let query = Book.find()
     if (req.query.title != null && req.query.title != '') {
       query = query.regex('title', new RegExp(req.query.title, 'i'))
@@ -42,63 +27,53 @@ router.get('/', async (req, res) => {
       res.redirect('/')
     }
   })
-
-// New Book Route
-router.get('/new', async (req, res) => {
+  
+  // New Book Route
+  router.get('/new', async (req, res) => {
     renderNewPage(res, new Book())
-})
-
-// Create Book Route
-router.post('/',upload.single('cover'), async (req, res) => {
-    // const fileName = req.file != null ? req.file.filename : null
+  })
+  
+  // Create Book Route
+  router.post('/', async (req, res) => {
     const book = new Book({
-        title: req.body.title,
-        author: req.body.author,
-        publisDate: new Date(req.body.publisDate),
-        pageCount: req.body.pageCount,
-        description: req.body.description
+      title: req.body.title,
+      author: req.body.author,
+      publishDate: new Date(req.body.publishDate),
+      pageCount: req.body.pageCount,
+      description: req.body.description
     })
     saveCover(book, req.body.cover)
-
+  
     try {
-        const newBook = book.save()
-         res.redirect(`books`)
-    } catch  {
-        if (book.coverImageName != null) {
-            removeBookCover(book.coverImageName)
-          }
-        renderNewPage(res, book,true)
-    }
-})
-
-// // BookCover Deletion
-// function removeBookCover(fileName) {
-//     fs.unlink(path.join(uploadPath, fileName), err => {
-//       if (err) console.error(err)
-//     })
-//   }
-
-// Render New Book Page
-async function renderNewPage(res, book, hasError = false){
-    try {
-        const authors = await Author.find({})
-        const params = {
-            authors: authors,
-            book: book
-        }
-        if (hasError) params.errorMessage = 'Error Creating Book'
-        res.render('books/new', params)
+      const newBook = await book.save()
+      // res.redirect(`books/${newBook.id}`)
+      res.redirect(`books`)
     } catch {
-        res.redirect('/books')
+      renderNewPage(res, book, true)
     }
-}
-
-function saveCover(book, coverEncoded){
-    if(coverEncoded == null) return
+  })
+  
+  async function renderNewPage(res, book, hasError = false) {
+    try {
+      const authors = await Author.find({})
+      const params = {
+        authors: authors,
+        book: book
+      }
+      if (hasError) params.errorMessage = 'Error Creating Book'
+      res.render('books/new', params)
+    } catch {
+      res.redirect('/books')
+    }
+  }
+  
+  function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
     const cover = JSON.parse(coverEncoded)
-    if(cover != null && imageMimeTypes.includes(cover.type)){
-        book.coverImage = new Buffer.from(cover.data, 'base64')
-        book.coverImageType = cover.type
-    } 
-}
-module.exports = router
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+      book.coverImage = new Buffer.from(cover.data, 'base64')
+      book.coverImageType = cover.type
+    }
+  }
+  
+  module.exports = router

f42eefb31acad6ade4903e3a9ce3d9afb5f1ce30
Akash Kaintura <akashkaintura@Akashs-MacBook-Pro.local>
2021-09-05 10:33:26 +0530

    fix: Images unable to render

--- models/author.js
+++ models/author.js
@@ -1,25 +1,25 @@
-const mongoose  = require("mongoose")
+const mongoose = require('mongoose')
 const Book = require('./book')
 
-// Add to Db
+// DB Validations
 const authorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true
+  }
 })
 
-// Schema Relationships
-authorSchema.pre('remove',function(next){
-    Book.find({ author: this.id }, (err, books) =>{
-        if(err){
-            next(err)
-        }else if(books.length > 0){
-            next(new Error('This author has books associated in Database'))
-        }else{
-            next()
-        }
-    })
+// DB Relations
+authorSchema.pre('remove', function(next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err)
+    } else if (books.length > 0) {
+      next(new Error('This author has books still'))
+    } else {
+      next()
+    }
+  })
 })
 
 module.exports = mongoose.model('Author', authorSchema)

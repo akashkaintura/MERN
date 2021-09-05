0154f55d88025f709d0cac016a62ba7ba35ac5b8
Akash Kaintura <akashkaintura@Akashs-MacBook-Pro.local>
2021-09-05 10:57:53 +0530

    update: CRUD-books

--- routes/books.js
+++ routes/books.js
@@ -52,6 +52,61 @@ router.post('/', async (req, res) => {
     renderNewPage(res, book, true)
   }
 })
+router.get('/:id', async (req, res) => {
+    try {
+      const book = await Book.findById(req.params.id).populate('author').exec()
+    //   const books = await Book.find({ author: author.id }).limit(6).exec()
+      res.render('books/show', {
+        book: books,
+        booksByAuthor: books
+      })
+    } catch {
+      res.redirect('/')
+    }
+  })
+  
+  router.get('/:id/edit', async (req, res) => {
+    try {
+      const author = await Author.findById(req.params.id)
+      res.render('authors/edit', { author: author })
+    } catch {
+      res.redirect('/authors')
+    }
+  })
+  
+  router.put('/:id', async (req, res) => {
+    let author
+    try {
+      author = await Author.findById(req.params.id)
+      author.name = req.body.name
+      await author.save()
+      res.redirect(`/authors/${author.id}`)
+    } catch {
+      if (author == null) {
+        res.redirect('/')
+      } else {
+        res.render('authors/edit', {
+          author: author,
+          errorMessage: 'Error updating Author'
+        })
+      }
+    }
+  })
+  
+  router.delete('/:id', async (req, res) => {
+    let author
+    try {
+      author = await Author.findById(req.params.id)
+      await author.delete()
+      res.redirect('/authors')
+    } catch {
+      if (author == null) {
+        res.redirect('/')
+      } else {
+        res.redirect(`/authors/${author.id}`)
+      }
+    }
+  })
 
 async function renderNewPage(res, book, hasError = false) {
   try {
